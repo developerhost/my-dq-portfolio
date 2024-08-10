43ef5ae9a62dfd748b8980742ce4f24804690d75
developerhost <kanonhara0609@gmail.com>
2024-08-11 00:43:52 +0900

    feat: フォントをインストール

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,13 +1,13 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
 
 export const Route = createLazyFileRoute('/')({
-  component: Index,
+  component: Home,
 });
 
-function Index() {
+function Home() {
   return (
     <div className="p-2">
-      <h2>Welcome Home!</h2>
+      <h1 className="text-4xl mb-8">橋田至のポートフォリオサイト</h1>
     </div>
   );
 }
