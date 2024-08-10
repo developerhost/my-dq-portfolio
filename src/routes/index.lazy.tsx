cabc7683db82acb225344db0a531ae752635cc4a
developerhost <kanonhara0609@gmail.com>
2024-08-11 00:21:30 +0900

    feat: tailwindを導入

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -7,7 +7,7 @@ export const Route = createLazyFileRoute('/')({
 function Index() {
   return (
     <div className="p-2">
-      <h3>Welcome Home!</h3>
+      <h2>Welcome Home!</h2>
     </div>
   );
 }
