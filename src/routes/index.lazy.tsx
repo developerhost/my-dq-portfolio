a6ef7a35632e5f05250985d4a765fab9f3466074
developerhost <kanonhara0609@gmail.com>
2024-08-11 01:22:37 +0900

    feat:タイプライターの追加

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,4 +1,5 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
+import { Typewriter } from 'react-simple-typewriter';
 
 export const Route = createLazyFileRoute('/')({
   component: Home,
@@ -6,8 +7,21 @@ export const Route = createLazyFileRoute('/')({
 
 function Home() {
   return (
-    <div className="p-2">
-      <h1 className="text-4xl mb-8">橋田至のポートフォリオサイト</h1>
+    <div className="p-2" style={{ whiteSpace: 'pre-line' }}>
+      <h1 className="text-4xl mb-8 text-center">
+        橋田至のポートフォリオサイト
+      </h1>
+      <div style={{ whiteSpace: 'pre-line' }}>
+        <Typewriter
+          words={[
+            'ようこそ、橋田至の冒険へ！\nここでは、私の冒険の記録を見ることができます。',
+          ]}
+          cursor
+          cursorStyle="_"
+          typeSpeed={70}
+          delaySpeed={1000}
+        />
+      </div>
     </div>
   );
 }
