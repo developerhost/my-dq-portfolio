ab647c4ea0b7136136fa1e98204d0fa1dad69097
xxxx <squall0609.kanon@gmail.com>
2024-08-12 16:53:53 +0900

    feat: 宝箱のメッセージを表示

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,10 @@
 export const TILES = {
-  FLOOR: 8,  // 床タイル
   HERO: 0,   // 勇者タイル
+  MURABITO: 1,  // 村人タイル
+  CAT: 2,   // 猫タイル
+  MAP_OUTSIDE: 3,  // マップ外
+  TREASURE_RED_GOLD: 4, // 宝箱オブジェクト①
+  TREASURE_GREEN_GOLD: 5, // 宝箱オブジェクト②
+  FLOOR: 8,  // 床タイル
   // 他のタイルも必要に応じて追加
 };
--- src/routes/room/-hooks/useMessage.ts
+++ src/routes/room/-hooks/useMessage.ts
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { TILES } from '@/constants';
 
 /**
  * メッセージを表示するためのカスタムフック
@@ -8,14 +9,20 @@ export function useMessage() {
 
   const handleTileClick = (type: number) => {
     switch (type) {
-      case 0:
+      case TILES.HERO:
         setMessage('こんにちは、私は橋田至です！');
         break;
-      case 1:
+      case TILES.MURABITO:
         setMessage('村人: ようこそ、冒険者！');
         break;
-      case 2:
+      case TILES.CAT:
         setMessage('猫: にゃーん');
+        break;
+      case TILES.TREASURE_RED_GOLD:
+        setMessage('TypeScriptを手に入れた!');
+        break;
+      case TILES.TREASURE_GREEN_GOLD:
+        setMessage('Reactを手に入れた!');
         break;
       default:
         setMessage('');
