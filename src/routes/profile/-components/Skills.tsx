2fda88125b0027cfadf56d0fc202b7a785dac157
developerhost <kanonhara0609@gmail.com>
2024-11-17 13:49:20 +0900

    feat: スキルを追加

--- src/routes/profile/-components/Skills.tsx
+++ src/routes/profile/-components/Skills.tsx
@@ -3,27 +3,53 @@ import {
   SiJavascript,
   SiReact,
   SiNextdotjs,
+  SiVuedotjs,
   SiGraphql,
   SiTailwindcss,
+  SiVitess,
+  SiNuxtdotjs,
+  SiVuetify,
+  SiJest,
+  SiVitest,
+  SiTrpc,
+  SiShadcnui,
+  SiPrisma,
+  SiNextdns,
+  SiZod,
+  SiRecoil,
 } from 'react-icons/si';
-import { FaHtml5, FaCss3Alt, FaUser } from 'react-icons/fa';
+import { FaHtml5, FaCss3Alt, FaCogs, FaAws, FaGithub } from 'react-icons/fa';
 
 const skills = [
   { name: 'TypeScript', icon: <SiTypescript /> },
   { name: 'JavaScript', icon: <SiJavascript /> },
   { name: 'React', icon: <SiReact /> },
   { name: 'Next.js', icon: <SiNextdotjs /> },
+  { name: 'Vue.js', icon: <SiVuedotjs /> },
+  { name: 'Nuxt.js', icon: <SiNuxtdotjs /> },
   { name: 'GraphQL', icon: <SiGraphql /> },
+  { name: 'Prisma', icon: <SiPrisma /> },
+  { name: 'Auth.js(NextAuth)', icon: <SiNextdns /> },
+  { name: 'Recoil', icon: <SiRecoil /> },
+  { name: 'zod', icon: <SiZod /> },
+  { name: 'Vite', icon: <SiVitess /> },
+  { name: 'Jest', icon: <SiJest /> },
+  { name: 'Vitest', icon: <SiVitest /> },
   { name: 'HTML', icon: <FaHtml5 /> },
   { name: 'CSS', icon: <FaCss3Alt /> },
   { name: 'TailWindCSS', icon: <SiTailwindcss /> },
+  { name: 'shadcn/ui', icon: <SiShadcnui /> },
+  { name: 'vuetify', icon: <SiVuetify /> },
+  { name: 'AWS', icon: <FaAws /> },
+  { name: 'GitHub', icon: <FaGithub /> },
+  { name: 'trpc', icon: <SiTrpc /> },
 ];
 
 export default function Skills() {
   return (
     <div className="bg-black border-2 border-white rounded-md p-6 w-72 mt-2">
       <div className="flex items-center justify-center mb-4">
-        <FaUser className="w-8 h-8" />
+        <FaCogs className="w-8 h-8" />
         <h2 className="text-xl font-bold ml-2">Skills</h2>
       </div>
       <div className="flex flex-wrap">
