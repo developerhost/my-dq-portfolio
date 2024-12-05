090dc1a09f8483c3edb3344fae535a7f188dc288
developerhost <kanonhara0609@gmail.com>
2024-12-05 21:01:21 +0900

    feat: ゲームコントローラーのデザインを改善し、Aボタンの機能を追加

--- src/routes/room/index.lazy.tsx
+++ src/routes/room/index.lazy.tsx
@@ -1,5 +1,83 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { createLazyFileRoute } from '@tanstack/react-router';
+
+import { Tile } from './-components/Tile';
+import { useMessage } from './-hooks/useMessage';
+
+import ChatMessage from '@/components/ChatMessage';
+import GameController from '@/components/GameController';
+import { TILES } from '@/constants';
+import { useHeroMovement } from '@/hooks/useHeroMovement';
+
+export const Room = () => {
+  const roomMap = [
+    [9, 9, 9, 9, 9, 9, 9, 9, 9],
+    [9, 4, 5, 8, 8, 8, 0, 6, 9],
+    [9, 8, 8, 8, 8, 8, 8, 8, 9],
+    [9, 8, 8, 8, 1, 8, 8, 2, 9],
+    [9, 9, 9, 9, 9, 9, 9, 9, 9],
+  ];
+
+  const initialPosition = { row: 1, col: 6 };
+
+  const { heroPosition, moveHero } = useHeroMovement(initialPosition, roomMap);
+  const {
+    message,
+    handleTileClick,
+    treasureRedGoldTaken,
+    treasureGreenGoldTaken,
+  } = useMessage();
+
+  const handleAButtonPress = () => {
+    alert('Aボタンが押されました！');
+    // Aボタンに対応する処理をここに追加
+  };
+
+  return (
+    <div className="min-h-screen bg-black text-white flex flex-col items-center">
+      {/* ゲーム画面 */}
+      <div className="relative w-full max-w-4xl aspect-video bg-black border-2 border-gray-700">
+        {/* タイル表示 */}
+        <div className="grid grid-cols-9 gap-0.5 w-full h-full">
+          {roomMap.flatMap((row, rowIndex) =>
+            row.map((tile, colIndex) => {
+              const isHeroPosition =
+                rowIndex === heroPosition.row && colIndex === heroPosition.col;
+              const isPreviousHeroPosition = roomMap[rowIndex][colIndex] === 0;
+
+              const type = isHeroPosition
+                ? TILES.HERO
+                : isPreviousHeroPosition
+                  ? TILES.FLOOR
+                  : tile;
+
+              return (
+                <div
+                  className="flex items-center justify-center bg-gray-800 border border-gray-700"
+                  key={`${rowIndex}-${colIndex}`}
+                >
+                  <Tile
+                    isTreasureGreenGoldTaken={treasureGreenGoldTaken}
+                    isTreasureRedGoldTaken={treasureRedGoldTaken}
+                    onClick={() => handleTileClick(type)}
+                    type={type}
+                  />
+                </div>
+              );
+            })
+          )}
+        </div>
+        {/* チャット表示 */}
+        {message && (
+          <div className="absolute bottom-4 left-1/2 -translate-x-1/2 w-4/5 bg-black bg-opacity-70 p-4 rounded border border-gray-500">
+            <ChatMessage message={message} />
+          </div>
+        )}
+      </div>
+      <GameController moveHero={moveHero} onAButtonPress={handleAButtonPress} />
+    </div>
+  );
+};
 
 export const Route = createLazyFileRoute('/room/')({
-  component: () => <div>Hello /room/!</div>
-})
+  component: Room,
+});
