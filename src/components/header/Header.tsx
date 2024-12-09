21cc488137e23d32993e861fdfcf6d4884289caf
developerhost <kanonhara0609@gmail.com>
2024-12-09 22:06:02 +0900

    feat: ヘッダーにBGMプレーヤーコンポーネントを追加

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,4 @@
+import { BgmPlayer } from '../BgmPlayer';
 import { AppSidebar } from './AppSidebar';
 import { PCHeader } from './PCHeader';
 
@@ -19,6 +20,7 @@ export const Header = () => {
         <div className="fixed left-0 w-full p-2 flex justify-between bg-opacity-10 shadow-md z-10 border-b-2 md:hidden border-white">
           <SidebarTrigger />
           <h1 className="text-lg font-bold">Hashida Portfolio</h1>
+          <BgmPlayer src="/bgm/8bit-jo-jokyoku.mp3" />
         </div>
       </div>
     </SidebarProvider>
