3aecc0989e8d77363c7383296747a74dddb93924
developerhost <kanonhara0609@gmail.com>
2024-12-13 16:57:23 +0900

    feat: 開発者ページに連絡先セクションを追加し、Twitterリンクを実装

--- src/routes/developer/-components/Developer.tsx
+++ src/routes/developer/-components/Developer.tsx
@@ -1,4 +1,10 @@
-import { FaHandHoldingHeart, FaTree } from 'react-icons/fa';
+import { Link } from '@tanstack/react-router';
+import {
+  FaEnvelope,
+  FaHandHoldingHeart,
+  FaTree,
+  FaTwitter,
+} from 'react-icons/fa';
 
 export const Developer = () => {
   const THOUGHT = [
@@ -30,7 +36,7 @@ export const Developer = () => {
     `・非同期処理における適切なエラースロー、logger設計`,
     `・trpcによるフロントエンドとバックエンドでの型共有`,
     `・zodやprismaからの型抽出`,
-    `・huskeyによる即時Feed Back, commit lint`,
+    `・huskeyによる即時Feed Back, commit lintの設定など`,
   ] as const;
 
   return (
@@ -54,7 +60,7 @@ export const Developer = () => {
       </div>
 
       {/* できること、やりたいこと */}
-      <div className="bg-black border-2 border-white rounded-md p-6 w-72">
+      <div className="bg-black border-2 border-white rounded-md p-6 w-72 mb-8">
         <div className="flex items-center justify-center mb-4">
           <FaTree className="w-8 h-8" />
           <h2 className="text-xl font-bold ml-2">できること、やりたいこと</h2>
@@ -68,6 +74,29 @@ export const Developer = () => {
               {action}
             </div>
           ))}
+        </div>
+      </div>
+
+      {/* Contact */}
+      <div className="bg-black border-2 border-white rounded-md p-6 w-72">
+        <div className="flex items-center justify-center mb-4">
+          <FaEnvelope className="w-8 h-8" />
+          <h2 className="text-xl font-bold ml-2">Contact</h2>
+        </div>
+        <div className="text-left">
+          <div className="flex items-center mb-2 text-left w-full bg-transparent border-none focus:outline-none">
+            もし副業としての参画をご依頼していただける方はX(Twitter)へDMをお願いいたします。
+            <br />
+            まずはお話を聞いてみたいなどのカジュアル面談に関しても大歓迎です。
+          </div>
+          <Link
+            className="flex items-center"
+            target="_blank"
+            to={'https://x.com/dall_develop' as unknown as '/'}
+          >
+            <FaTwitter className="w-4 h-4 mr-2" />
+            Twitter: @dall_develop
+          </Link>
         </div>
       </div>
     </div>
