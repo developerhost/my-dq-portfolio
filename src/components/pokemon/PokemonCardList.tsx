bb69c6322c1c8f3f4eb7b8fdd431051d2ae70322
developerhost <kanonhara0609@gmail.com>
2025-01-19 20:35:27 +0900

    feat: ポケモンカードリストを更新し、カード情報を日本語に変更

--- src/components/pokemon/PokemonCardList.tsx
+++ src/components/pokemon/PokemonCardList.tsx
@@ -12,7 +12,7 @@ type PokemonCardListProps = {
 
 const PokemonCardList = ({ cards }: PokemonCardListProps) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-4">
       {cards.map((card) => (
         <PokemonCard card={card} key={card.id} />
       ))}
--- src/components/pokemon/cards.ts
+++ src/components/pokemon/cards.ts
@@ -1,23 +1,24 @@
 export const pokemonCards = [
   {
     id: '1',
-    name: 'Pikachu',
-    description: 'Electric-type Pokémon',
-    img: 'https://images.pokemontcg.io/base1/58_hires.png',
+    name: 'ミュウ',
+    description: '釣り竿で釣ることができる',
+    img: 'https://images.pokemontcg.io/swsh8/269_hires.png',
     back: 'https://tcg.pokemon.com/assets/img/global/tcg-card-back-2x.jpg',
   },
   {
     id: '2',
-    name: 'Charizard',
-    description: 'Fire-type Pokémon',
-    img: 'https://images.pokemontcg.io/base2/4_hires.png',
+    name: 'ミュウツー',
+    description: 'ミュウの遺伝子から生み出された',
+    img: 'https://images.pokemontcg.io/swshp/SWSH229_hires.png',
     back: 'https://tcg.pokemon.com/assets/img/global/tcg-card-back-2x.jpg',
   },
   {
     id: '3',
-    name: 'Blastoise',
-    description: 'Water-type Pokémon',
-    img: 'https://images.pokemontcg.io/base1/2_hires.png',
+    name: 'ゲンガー',
+    description:
+      '暗闇で遭難した人の命を狙い、影に潜り込んでチャンスをうかがう。様々な都市伝説がある。',
+    img: 'https://images.pokemontcg.io/swsh1/85_hires.png',
     back: 'https://tcg.pokemon.com/assets/img/global/tcg-card-back-2x.jpg',
   },
   // 他のカードを追加
