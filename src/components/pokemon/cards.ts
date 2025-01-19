86e2e6762e86292145e0470fe5b5bbe0cd484846
developerhost <kanonhara0609@gmail.com>
2025-01-19 18:48:31 +0900

    feat: ポートフォリオにポケモンカードリストを追加し、カードコンポーネントを実装

---
+++ src/components/pokemon/cards.ts
@@ -0,0 +1,24 @@
+export const pokemonCards = [
+  {
+    id: '1',
+    name: 'Pikachu',
+    description: 'Electric-type Pokémon',
+    img: 'https://images.pokemontcg.io/base1/58_hires.png',
+    back: 'https://tcg.pokemon.com/assets/img/global/tcg-card-back-2x.jpg',
+  },
+  {
+    id: '2',
+    name: 'Charizard',
+    description: 'Fire-type Pokémon',
+    img: 'https://images.pokemontcg.io/base2/4_hires.png',
+    back: 'https://tcg.pokemon.com/assets/img/global/tcg-card-back-2x.jpg',
+  },
+  {
+    id: '3',
+    name: 'Blastoise',
+    description: 'Water-type Pokémon',
+    img: 'https://images.pokemontcg.io/base1/2_hires.png',
+    back: 'https://tcg.pokemon.com/assets/img/global/tcg-card-back-2x.jpg',
+  },
+  // 他のカードを追加
+];
