34394e88095d4a4b54e1cd87a72ca3d67f5894a1
developerhost <kanonhara0609@gmail.com>
2025-02-26 12:17:09 +0900

    fix

--- src/hooks/useTracking.ts
+++ src/hooks/useTracking.ts
@@ -6,9 +6,12 @@ import ReactGA from 'react-ga4';
 const TRACKING_ID = 'G-FSSXRWL3SD'; // あなたの Google Analytics 測定 ID
 
 const usePageTracking = () => {
-  const { location } = useRouterState();
+  const routerState = useRouterState() as {
+    location: { pathname: string; searchStr?: string };
+  };
+  const { location } = routerState;
   const pathname = location.pathname;
-  const search = location.searchStr; // `search` の型エラーを避けるため `searchStr` を使用
+  const search = location.searchStr ?? ''; // `search` の型エラーを避けるため `searchStr` を使用
 
   useEffect(() => {
     // Google Analytics の初期化は一度だけ行う
