f395b0966b7e28650a83e0fb6462eb461fe1b493
developerhost <kanonhara0609@gmail.com>
2025-02-26 12:36:17 +0900

    fix link to

--- src/routes/blog/index.lazy.tsx
+++ src/routes/blog/index.lazy.tsx
@@ -21,10 +21,12 @@ export const BlogList = () => {
           <li className="mb-4" key={post.slug}>
             <Link
               className="text-blue-500 hover:underline"
-              to={`/blog/${post.slug}` as const}
+              params={{ slug: post.slug }}
+              to="/blog/$slug"
             >
               <h2 className="text-xl font-bold">{post.title}</h2>
             </Link>
+
             <p className="text-gray-500">{post.date}</p>
             <p>{post.excerpt}</p>
           </li>
