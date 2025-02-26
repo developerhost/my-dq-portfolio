12b29414032d9ac942b249ef50fbb78acc8310aa
developerhost <kanonhara0609@gmail.com>
2025-02-26 12:09:41 +0900

    fix useLocation

--- src/hooks/useTracking.ts
+++ src/hooks/useTracking.ts
@@ -1,12 +1,14 @@
 import { useEffect } from 'react';
 
-import { useLocation } from '@tanstack/react-router';
+import { useRouterState } from '@tanstack/react-router';
 import ReactGA from 'react-ga4';
 
 const TRACKING_ID = 'G-FSSXRWL3SD'; // あなたの Google Analytics 測定 ID
 
 const usePageTracking = () => {
-  const location = useLocation();
+  const { location } = useRouterState();
+  const pathname = location.pathname;
+  const search = location.searchStr; // `search` の型エラーを避けるため `searchStr` を使用
 
   useEffect(() => {
     // Google Analytics の初期化は一度だけ行う
@@ -17,9 +19,9 @@ const usePageTracking = () => {
     // ページビューを送信
     ReactGA.send({
       hitType: 'pageview',
-      page: location.pathname + location.search,
+      page: pathname + search,
     });
-  }, [location]);
+  }, [pathname, search]);
 };
 
 export default usePageTracking;
