938196e2d42279b173aa64f6847a2d8c1acb4a2d
developerhost <kanonhara0609@gmail.com>
2025-03-16 15:28:09 +0900

    共通のアーティストカードコンポーネントを作成し、MusicPlayerコンポーネントに統合

--- src/components/MusicPlayer/MusicPlayer.tsx
+++ src/components/MusicPlayer/MusicPlayer.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-sort-props */
 import { useState, useEffect, useCallback } from 'react';
 
+import { ArtistCard } from './ArtistCard';
 import { CLIENT_ID, CLIENT_SECRET } from './constants';
 
 import type { Track, ArtistDetails } from './type';
@@ -16,7 +17,6 @@ const AVICII_NAME = 'Avicii';
 const MRCHILDREN_NAME = 'Mr.Children';
 
 const MusicPlayer = () => {
-  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
   const [aviciiData, setAviciiData] = useState<{
     artist: ArtistDetails;
     topTrack: Track;
@@ -123,60 +123,6 @@ const MusicPlayer = () => {
     };
     fetchData();
   }, [fetchTopTrack]);
-
-  const handlePlay = (previewUrl: string) => {
-    if (audio) {
-      audio.pause();
-    }
-    const newAudio = new Audio(previewUrl);
-    setAudio(newAudio);
-    newAudio.play();
-  };
-
-  // 共通化されたアーティストカードコンポーネント
-  const ArtistCard = ({
-    title,
-    data,
-  }: {
-    data: { artist: ArtistDetails; topTrack: Track };
-    title: string;
-  }) => (
-    <div className="mb-4 p-4 border rounded shadow">
-      <h2 className="text-xl font-semibold">{title}</h2>
-      {data.artist.images && data.artist.images[0] && (
-        <img
-          alt={data.artist.name}
-          className="w-32 h-32 object-cover mb-2"
-          src={data.artist.images[0].url}
-        />
-      )}
-      <p>
-        <strong>トップトラック:</strong> {data.topTrack.name}
-      </p>
-      {data.topTrack.album.images && data.topTrack.album.images[0] && (
-        <img
-          alt={data.topTrack.name}
-          className="w-full h-48 object-cover mb-2"
-          src={data.topTrack.album.images[0].url}
-        />
-      )}
-      {data.topTrack.preview_url ? (
-        <button
-          className="bg-blue-500 hover:bg-blue-600 mt-2 px-4 py-2 rounded text-white"
-          onClick={() => {
-            const url = data.topTrack.preview_url;
-            if (url) {
-              handlePlay(url);
-            }
-          }}
-        >
-          再生
-        </button>
-      ) : (
-        <p className="mt-2 text-red-500">♫</p>
-      )}
-    </div>
-  );
 
   return (
     <div className="p-4">
